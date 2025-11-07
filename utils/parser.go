cd683d8218dd39308f0c8e8f4db42c70a495f526
Sadiq <pageton@proton.me>
2025-11-07 23:24:36 +0300

    feat(utils): add ParseEmailAddresses helper function

--- utils/parser.go
+++ utils/parser.go
@@ -1,2 +1,26 @@
 // Package utils contains the utility functions for the application.
 package utils
+
+import (
+	"log"
+	"net/mail"
+)
+
+func ParseEmailAddresses(header string) []string {
+	if header == "" {
+		return nil
+	}
+
+	addrs, err := mail.ParseAddressList(header)
+	if err != nil {
+		log.Println("Error parsing addresses:", err)
+		return nil
+	}
+
+	var result []string
+	for _, addr := range addrs {
+		result = append(result, addr.Address)
+	}
+
+	return result
+}
