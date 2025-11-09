619de9edeba028041fec9fa06aca3bdc8135052e
Sadiq <pageton@proton.me>
2025-11-09 03:50:47 +0300

    chore(middleware): add CORS helper middleware

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,2 +1,11 @@
 // Package middlewares contains the middlewares for the application.
 package middlewares
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func Cors(app *fiber.App) {
+	app.Use(cors.New())
+}
