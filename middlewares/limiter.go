8b41bb3013b0c09f6b9cde11ffb9cc9626b5c865
Sadiq <pageton@proton.me>
2025-11-09 03:54:33 +0300

    chore(middleware): add rate limiter middleware

---
+++ middlewares/limiter.go
@@ -0,0 +1,22 @@
+// Package middlewares contains the middlewares for the application.
+package middlewares
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+)
+
+func RateLimiter(app *fiber.App) {
+	app.Use(limiter.New(limiter.Config{
+		Max:        40,
+		Expiration: 1 * time.Minute,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.IP()
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.SendStatus(fiber.StatusTooManyRequests)
+		},
+	}))
+}
